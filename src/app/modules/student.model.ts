e439ca90ad47296c26cf2469e77e92bfbb35bfea
Nizam Shejan <nizam.shejan11@gmail.com>
2023-11-21 17:32:08 +0600

    student created successfully

---
+++ src/app/modules/student.model.ts
@@ -0,0 +1,70 @@
+import { Schema, model } from "mongoose";
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from "./student/student.interface";
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+    required: true,
+  },
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: {
+    type: String,
+    enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: {
+    type: String,
+    enum: ["active", "blocked"],
+    default: "active",
+  },
+});
+
+// create model
+
+export const StudentModel = model<Student>("Student", studentSchema);
